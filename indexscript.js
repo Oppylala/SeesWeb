4a373153fcbe9c14f097a7268183ef0500f17173
Adesina Opeyemi <49338483+Oppylala@users.noreply.github.com>
2020-09-19 10:36:20 +0100

    Add files via upload

--- indexscript.js
+++ indexscript.js
@@ -72,9 +72,63 @@ function validateCreate(){
 	}
 	
 	else{
-		firebase.document().ref("Users").set({
-			
-		});
+		
+		var firebaseref = new Firebase("https://textpage-67274.firebaseio.com")
+
+$("#login-btn").on('click', function() 
+{
+        var email = $("#login-email").val();
+        var password = $("#login-password").val();
+        firebaseref.authWithPassword({
+            email: email,
+            password: password
+        }, 
+        function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+            }
+        });
+});
+
+$("#signup-btn").on('click', function() 
+{
+	var email = $("#email").val();
+	var password = $("#password").val();
+	firebaseref.createUser({
+		email: email,
+		password: password
+	},function(error, userData) {
+		if (error) {
+			console.log("Error creating user:", error);
+		} 
+		else {
+			console.log("Successfully created user account with uid:", userData.uid);
+			//additionally, you can log the user in right after the signup is successful and add more data about the user like name etc.              
+		}
+	});
+});
+//Callback for Auth Changes
+var authDataCallback = function(authData) 
+{
+        //authData is the object sent by Firebase in the callback.
+        if (authData) {
+            console.log("User " + authData.uid + " is logged in");
+        } 
+        else {
+            console.log("User is logged out");
+        }
+}
+
+//register a callback for the change in Authentication Status
+firebaseref.onAuth(authDataCallback);
+		
+		
+		
+		
+		
+		
 	}
 }
 function createValidate(){
@@ -88,7 +142,6 @@ function createValidate(){
 		alert("Your name must not be empty");
 		document.getElementById('name').focus();
 		return false;
-		
 	}
 	else if(email == ""){
 		alert("Your email is required !");
